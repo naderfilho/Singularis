f73068656ece7996f90d713d7dad1a01c10e65d3
Nader Filho <ndr.dev@outlook.com>
2026-09-22 10:04:17 -0300

    fix(quantum): avoid circular import of comparison module

--- semente/quantum/__init__.py
+++ semente/quantum/__init__.py
@@ -1,3 +1,2 @@
 from .einstein_cartan import EinsteinCartanCorrection, bounce_density_planck, bounce_density_si  # noqa: F401
 from .lqc import LQCCorrection, RHO_C_PLANCK  # noqa: F401
-from .comparison import compare_models, dust_equivalence_residual, radiation_difference  # noqa: F401
